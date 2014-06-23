0565873631d32672255b55f313d4224cd8aa1695
alexis <alexis@vgr-debian-4.(none)>
2014-06-24 01:08:39 +0300

    the second commit

--- ui.R
+++ ui.R
@@ -2,12 +2,14 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("The Weight Calculator"),  
   sidebarPanel(    
-    div("This is a simple calculator of weight."),
-    div("Input your Gender and Height."),
+    div("This is a simple calculator of weight"),
+    div("Define your gender and your height"),
     div("Robinson formula, Hamwi formula and health recommendations are going to be used"),
     br(), 
-    radioButtons("sex", label = h4("Gender:"), choices = list("Male" = 1, "Female" = 2), selected = 1),  
-    sliderInput("height", label = h4("Height (cm):"), min=100, max=230, value=175)    
+    radioButtons("sex", label = h4("Gender:"),
+                 choices = list("Male" = 1, "Female" = 2), selected = 1),  
+    sliderInput("height", label = h4("Height (cm):"),
+                min=100, max=230, value=175)    
   ),
   mainPanel(
     h3("Results"),
